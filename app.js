1165dbc4e930e12ef976fbe4697377c315ad3c01
AndreyMebel <154588828+AndreyMebel@users.noreply.github.com>
2023-12-22 18:38:37 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,519 @@
+let allCount = document.querySelector('.summ-flex-count');
+
+
+
+
+let elemCount1 = document.querySelector('.elem1-count');
+let plus1 = document.querySelector('.elem1-plus');
+let minus1 = document.querySelector('.elem1-minus');
+let input1 = document.querySelector('.elem1-input');
+
+plus1.addEventListener('click', () => {
+    if(+elemCount1.textContent !== 20) {
+        let count = +elemCount1.textContent + 1;
+        elemCount1.textContent = count;  
+    } 
+})
+
+minus1.addEventListener('click', () => {
+    if(+elemCount1.textContent !== 0) {
+        let count = +elemCount1.textContent - 1;
+        elemCount1.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input1.value = `${+elemCount1.textContent * 3000}`;
+})
+
+let elemCount2 = document.querySelector('.elem2-count');
+let plus2 = document.querySelector('.elem2-plus');
+let minus2 = document.querySelector('.elem2-minus');
+let input2 = document.querySelector('.elem2-input');
+
+plus2.addEventListener('click', () => {
+    if(+elemCount2.textContent !== 20) {
+        let count = +elemCount2.textContent + 1;
+        elemCount2.textContent = count;  
+    } 
+})
+
+minus2.addEventListener('click', () => {
+    if(+elemCount2.textContent !== 0) {
+        let count = +elemCount2.textContent - 1;
+        elemCount2.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input2.value = `${+elemCount2.textContent * 1500}`;
+})
+
+let elemCount3 = document.querySelector('.elem3-count');
+let plus3 = document.querySelector('.elem3-plus');
+let minus3 = document.querySelector('.elem3-minus');
+let input3 = document.querySelector('.elem3-input');
+
+plus3.addEventListener('click', () => {
+    if(+elemCount3.textContent !== 20) {
+        let count = +elemCount3.textContent + 1;
+        elemCount3.textContent = count;  
+    } 
+})
+
+minus3.addEventListener('click', () => {
+    if(+elemCount3.textContent !== 0) {
+        let count = +elemCount3.textContent - 1;
+        elemCount3.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input3.value = `${+elemCount3.textContent * 25}`;
+})
+
+let elemCount4 = document.querySelector('.elem4-count');
+let plus4 = document.querySelector('.elem4-plus');
+let minus4 = document.querySelector('.elem4-minus');
+let input4 = document.querySelector('.elem4-input');
+
+plus4.addEventListener('click', () => {
+    if(+elemCount4.textContent !== 20) {
+        let count = +elemCount4.textContent + 1;
+        elemCount4.textContent = count;  
+    } 
+})
+
+minus4.addEventListener('click', () => {
+    if(+elemCount4.textContent !== 0) {
+        let count = +elemCount4.textContent - 1;
+        elemCount4.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input4.value = `${+elemCount4.textContent * 5000}`;
+})
+
+let elemCount5 = document.querySelector('.elem5-count');
+let plus5 = document.querySelector('.elem5-plus');
+let minus5 = document.querySelector('.elem5-minus');
+let input5 = document.querySelector('.elem5-input');
+
+plus5.addEventListener('click', () => {
+    if(+elemCount5.textContent !== 20) {
+        let count = +elemCount5.textContent + 1;
+        elemCount5.textContent = count;  
+    } 
+})
+
+minus5.addEventListener('click', () => {
+    if(+elemCount5.textContent !== 0) {
+        let count = +elemCount5.textContent - 1;
+        elemCount5.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input5.value = `${+elemCount5.textContent * 2500}`;
+})
+
+let elemCount6 = document.querySelector('.elem6-count');
+let plus6 = document.querySelector('.elem6-plus');
+let minus6 = document.querySelector('.elem6-minus');
+let input6 = document.querySelector('.elem6-input');
+
+plus6.addEventListener('click', () => {
+    if(+elemCount6.textContent !== 20) {
+        let count = +elemCount6.textContent + 1;
+        elemCount6.textContent = count;  
+    } 
+})
+
+minus6.addEventListener('click', () => {
+    if(+elemCount6.textContent !== 0) {
+        let count = +elemCount6.textContent - 1;
+        elemCount6.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input6.value = `${+elemCount6.textContent * 6000}`;
+})
+
+let elemCount7 = document.querySelector('.elem7-count');
+let plus7 = document.querySelector('.elem7-plus');
+let minus7 = document.querySelector('.elem7-minus');
+let input7 = document.querySelector('.elem7-input');
+
+plus7.addEventListener('click', () => {
+    if(+elemCount7.textContent !== 20) {
+        let count = +elemCount7.textContent + 1;
+        elemCount7.textContent = count;  
+    } 
+})
+
+minus7.addEventListener('click', () => {
+    if(+elemCount7.textContent !== 0) {
+        let count = +elemCount7.textContent - 1;
+        elemCount7.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input7.value = `${+elemCount7.textContent * 6000}`;
+})
+
+let elemCount8 = document.querySelector('.elem8-count');
+let plus8 = document.querySelector('.elem8-plus');
+let minus8 = document.querySelector('.elem8-minus');
+let input8 = document.querySelector('.elem8-input');
+
+plus8.addEventListener('click', () => {
+    if(+elemCount8.textContent !== 20) {
+        let count = +elemCount8.textContent + 1;
+        elemCount8.textContent = count;  
+    } 
+})
+
+minus8.addEventListener('click', () => {
+    if(+elemCount8.textContent !== 0) {
+        let count = +elemCount8.textContent - 1;
+        elemCount8.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input8.value = `${+elemCount8.textContent * 1500}`;
+})
+
+let elemCount9 = document.querySelector('.elem9-count');
+let plus9 = document.querySelector('.elem9-plus');
+let minus9 = document.querySelector('.elem9-minus');
+let input9 = document.querySelector('.elem9-input');
+
+plus9.addEventListener('click', () => {
+    if(+elemCount9.textContent !== 20) {
+        let count = +elemCount9.textContent + 1;
+        elemCount9.textContent = count;  
+    } 
+})
+
+minus9.addEventListener('click', () => {
+    if(+elemCount9.textContent !== 0) {
+        let count = +elemCount9.textContent - 1;
+        elemCount9.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input9.value = `${+elemCount9.textContent * 2000}`;
+})
+
+let elemCount10 = document.querySelector('.elem10-count');
+let plus10 = document.querySelector('.elem10-plus');
+let minus10 = document.querySelector('.elem10-minus');
+let input10 = document.querySelector('.elem10-input');
+
+plus10.addEventListener('click', () => {
+    if(+elemCount10.textContent !== 20) {
+        let count = +elemCount10.textContent + 1;
+        elemCount10.textContent = count;  
+    } 
+})
+
+minus10.addEventListener('click', () => {
+    if(+elemCount10.textContent !== 0) {
+        let count = +elemCount10.textContent - 1;
+        elemCount10.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input10.value = `${+elemCount10.textContent * 7000}`;
+})
+
+let elemCount11 = document.querySelector('.elem11-count');
+let plus11 = document.querySelector('.elem11-plus');
+let minus11 = document.querySelector('.elem11-minus');
+let input11 = document.querySelector('.elem11-input');
+
+plus11.addEventListener('click', () => {
+    if(+elemCount11.textContent !== 20) {
+        let count = +elemCount11.textContent + 1;
+        elemCount11.textContent = count;  
+    } 
+})
+
+minus11.addEventListener('click', () => {
+    if(+elemCount11.textContent !== 0) {
+        let count = +elemCount11.textContent - 1;
+        elemCount11.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input11.value = `${+elemCount11.textContent * 2500}`;
+})
+
+let elemCount12 = document.querySelector('.elem12-count');
+let plus12 = document.querySelector('.elem12-plus');
+let minus12 = document.querySelector('.elem12-minus');
+let input12 = document.querySelector('.elem12-input');
+
+plus12.addEventListener('click', () => {
+    if(+elemCount12.textContent !== 20) {
+        let count = +elemCount12.textContent + 1;
+        elemCount12.textContent = count;  
+    } 
+})
+
+minus12.addEventListener('click', () => {
+    if(+elemCount12.textContent !== 0) {
+        let count = +elemCount12.textContent - 1;
+        elemCount12.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input12.value = `${+elemCount12.textContent * 1000}`;
+})
+
+let elemCount13 = document.querySelector('.elem13-count');
+let plus13 = document.querySelector('.elem13-plus');
+let minus13 = document.querySelector('.elem13-minus');
+let input13 = document.querySelector('.elem13-input');
+
+plus13.addEventListener('click', () => {
+    if(+elemCount13.textContent !== 20) {
+        let count = +elemCount13.textContent + 1;
+        elemCount13.textContent = count;
+    } 
+})
+
+minus13.addEventListener('click', () => {
+    if(+elemCount13.textContent !== 0) {
+        let count = +elemCount13.textContent - 1;
+        elemCount13.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input13.value = `${+elemCount13.textContent * 400}`;
+})
+
+let elemCount14 = document.querySelector('.elem14-count');
+let plus14 = document.querySelector('.elem14-plus');
+let minus14 = document.querySelector('.elem14-minus');
+let input14 = document.querySelector('.elem14-input');
+
+plus14.addEventListener('click', () => {
+    if(+elemCount14.textContent !== 20) {
+        let count = +elemCount14.textContent + 1;
+        elemCount14.textContent = count;
+    } 
+})
+
+minus14.addEventListener('click', () => {
+    if(+elemCount14.textContent !== 0) {
+        let count = +elemCount14.textContent - 1;
+        elemCount14.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input14.value = `${+elemCount14.textContent * 100}`;
+})
+
+let elemCount15 = document.querySelector('.elem15-count');
+let plus15 = document.querySelector('.elem15-plus');
+let minus15 = document.querySelector('.elem15-minus');
+let input15 = document.querySelector('.elem15-input');
+
+plus15.addEventListener('click', () => {
+    if(+elemCount15.textContent !== 20) {
+        let count = +elemCount15.textContent + 1;
+        elemCount15.textContent = count;
+    } 
+})
+
+minus15.addEventListener('click', () => {
+    if(+elemCount15.textContent !== 0) {
+        let count = +elemCount15.textContent - 1;
+        elemCount15.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input15.value = `${+elemCount15.textContent * 350}`;
+})
+
+let elemCount16 = document.querySelector('.elem16-count');
+let plus16 = document.querySelector('.elem16-plus');
+let minus16 = document.querySelector('.elem16-minus');
+let input16 = document.querySelector('.elem16-input');
+
+plus16.addEventListener('click', () => {
+    if(+elemCount16.textContent !== 20) {
+        let count = +elemCount16.textContent + 1;
+        elemCount16.textContent = count;
+    } 
+})
+
+minus16.addEventListener('click', () => {
+    if(+elemCount16.textContent !== 0) {
+        let count = +elemCount16.textContent - 1;
+        elemCount16.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input16.value = `${+elemCount16.textContent * 1500}`;
+})
+
+let elemCount17 = document.querySelector('.elem17-count');
+let plus17 = document.querySelector('.elem17-plus');
+let minus17 = document.querySelector('.elem17-minus');
+let input17 = document.querySelector('.elem17-input');
+
+plus17.addEventListener('click', () => {
+    if(+elemCount17.textContent !== 20) {
+        let count = +elemCount17.textContent + 1;
+        elemCount17.textContent = count;
+    } 
+})
+
+minus17.addEventListener('click', () => {
+    if(+elemCount17.textContent !== 0) {
+        let count = +elemCount17.textContent - 1;
+        elemCount17.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input17.value = `${+elemCount17.textContent * 1750}`;
+})
+
+let elemCount18 = document.querySelector('.elem18-count');
+let plus18 = document.querySelector('.elem18-plus');
+let minus18 = document.querySelector('.elem18-minus');
+let input18 = document.querySelector('.elem18-input');
+
+plus18.addEventListener('click', () => {
+    if(+elemCount18.textContent !== 20) {
+        let count = +elemCount18.textContent + 1;
+        elemCount18.textContent = count;
+    } 
+})
+
+minus18.addEventListener('click', () => {
+    if(+elemCount18.textContent !== 0) {
+        let count = +elemCount18.textContent - 1;
+        elemCount18.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input18.value = `${+elemCount18.textContent * 250}`;
+})
+
+let elemCount19 = document.querySelector('.elem19-count');
+let plus19 = document.querySelector('.elem19-plus');
+let minus19 = document.querySelector('.elem19-minus');
+let input19 = document.querySelector('.elem19-input');
+
+plus19.addEventListener('click', () => {
+    if(+elemCount19.textContent !== 20) {
+        let count = +elemCount19.textContent + 1;
+        elemCount19.textContent = count;
+    } 
+})
+
+minus19.addEventListener('click', () => {
+    if(+elemCount19.textContent !== 0) {
+        let count = +elemCount19.textContent - 1;
+        elemCount19.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input19.value = `${+elemCount19.textContent * 20}`;
+})
+
+let elemCount20 = document.querySelector('.elem20-count');
+let plus20 = document.querySelector('.elem20-plus');
+let minus20 = document.querySelector('.elem20-minus');
+let input20 = document.querySelector('.elem20-input');
+
+plus20.addEventListener('click', () => {
+    if(+elemCount20.textContent !== 20) {
+        let count = +elemCount20.textContent + 1;
+        elemCount20.textContent = count;
+    } 
+})
+
+minus20.addEventListener('click', () => {
+    if(+elemCount20.textContent !== 0) {
+        let count = +elemCount20.textContent - 1;
+        elemCount20.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input20.value = `${+elemCount20.textContent * 7000}`;
+})
+
+let elemCount21 = document.querySelector('.elem21-count');
+let plus21 = document.querySelector('.elem21-plus');
+let minus21 = document.querySelector('.elem21-minus');
+let input21 = document.querySelector('.elem21-input');
+
+plus21.addEventListener('click', () => {
+    if(+elemCount21.textContent !== 20) {
+        let count = +elemCount21.textContent + 1;
+        elemCount21.textContent = count;
+    } 
+})
+
+minus21.addEventListener('click', () => {
+    if(+elemCount21.textContent !== 0) {
+        let count = +elemCount21.textContent - 1;
+        elemCount21.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input21.value = `${+elemCount21.textContent * 7000}`;
+})
+
+let elemCount22 = document.querySelector('.elem22-count');
+let plus22 = document.querySelector('.elem22-plus');
+let minus22 = document.querySelector('.elem22-minus');
+let input22 = document.querySelector('.elem22-input');
+
+plus22.addEventListener('click', () => {
+    if(+elemCount22.textContent !== 20) {
+        let count = +elemCount22.textContent + 1;
+        elemCount22.textContent = count;
+    } 
+})
+
+minus22.addEventListener('click', () => {
+    if(+elemCount22.textContent !== 0) {
+        let count = +elemCount22.textContent - 1;
+        elemCount22.textContent = count;
+    }
+})
+
+document.addEventListener('click', () => {
+    input22.value = `${+elemCount22.textContent * 1000}`;
+})
+
+document.addEventListener('click', () => {
+    allCount.textContent = `${+input1.value + +input2.value + +input3.value + +input4.value + +input5.value + +input6.value
+    + +input7.value + +input8.value + +input9.value + +input10.value + +input11.value + +input12.value + +input13.value
+    + +input14.value + +input15.value + +input16.value + +input17.value + +input18.value + +input19.value + +input20.value
+    + +input21.value + +input22.value} руб.`
+})
+
+
